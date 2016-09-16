89d34430f87113dfcedb59724b9456209ab821aa
doowb <brian.woodward@gmail.com>
2016-09-16 11:12:06 -0400

    add documentation code comments initialize verb generate readme documentation with verb

--- index.js
+++ index.js
@@ -6,7 +6,31 @@ var extend = require('extend-shallow');
 var parse = require('parse-github-url');
 var getUrl = require('get-repository-url');
 
-module.exports = function(repo, options, cb) {
+/**
+ * Get the last 14 days of Github traffic for the specified repository.
+ *
+ * ```js
+ * var options = {
+ *   username: 'doowb',
+ *   password: '**********'
+ * };
+ *
+ * traffic('assemble/assemble', options, function(err, results) {
+ *   if (err) return console.error(err);
+ *   console.log(results);
+ * });
+ * ```
+ * @param  {String} `repo` Full repository to get traffic, formatted as `:owner/:repo`. If the repository is published to NPM, the NPM name may be used.
+ * @param  {Object} `options` Options containing the Github authentication information. This is required since user's must be administrators on the repository to retrieve traffic information.
+ * @param  {String} `options.username` Github username of the repository administrator retrieving the traffic information. This is required if an oauth token is not used.
+ * @param  {String} `options.password` Github user's password of the repository administrator retrieving the traffic information. This is required if an oauth token is not used.
+ * @param  {String} `options.token` Github oauth token for the repository administrator retrieving the traffic informatino. This is required if the `username/password` combination is not used.
+ * @param  {Function} `cb` Optional callback function that will be called with error information or the results. When omitted a Promise is returned.
+ * @return {Promise} Promise with the traffic information when ready.
+ * @api public
+ */
+
+module.exports = function traffic(repo, options, cb) {
   if (typeof options === 'function') {
     cb = options;
     options = {};
